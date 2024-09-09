8fe93a8578724f45da3bfcf04d9aa3e45ff93a53
rohitkushrbj7 <rohitkush.rbj7@gmail.com>
2024-09-09 23:56:13 +0530

    asdas

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ inject();
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://chetankesare9.vercel.app/"),
+	metadataBase: new URL("https://vercel.com/rohitkushrbj7s-projects"),
 
-	title: 'Chetan Kesare',
+	title: 'Rohit Kumar',
 	authors: {
 		name: "Chetan Kesare",
 	},
@@ -20,12 +20,12 @@ export const metadata: Metadata = {
 	description:
 		"Evolving Code, Self-Taught Spirit: Web Development Journey from India.",
 	openGraph: {
-		title: "Chetan Kesare",
+		title: "Rohit Kumar",
 		description:
 			"Evolving Code, Self-Taught Spirit: Web Development Journey from India.",
 		url: "https://chetankesare9.vercel.app/",
-		siteName: "Chetan Kesare",
-		images: "/newPortfolio.png",
+		siteName: "Rohit Kumar",
+		// images: "/newPortfolio.png",
 		type: "website",
 	},
 	keywords: ["personal portfolio website", "portfolio website templete", "web developer", "web Development roadmap", "react portfolio website", "3d portfolio website", "three.js portfolio website", "portfolio website github","portfolio website template","free portfolio website", "chetan kesare", "chetan kesare github", "chetan kesare linkedin", "chetan kesare from sangli", "terna engineering college nerul", "terna college", "terna college mumbai", "terna dental college"],
