f7e6ea12a4f4a4c241e1e90c9715f5205b9bc48c
Rohit Kumar <125146333+rohitkushrbj7@users.noreply.github.com>
2024-12-14 11:30:31 +0530

    Update moving-border.tsx

--- components/ui/moving-border.tsx
+++ components/ui/moving-border.tsx
@@ -38,7 +38,7 @@ export function MovingBorderBtn({
 
   const handleDownloadResume = () => {
     
-    const resumeUrl = 'https://drive.google.com/file/d/10GsEPfbdUgZMbg90ySPoRK0NIXQSuGTT/view?usp=sharing';
+    const resumeUrl = 'https://drive.google.com/file/d/1n5g0k0U-eqL0WCU8fHGzoTjEOFI4RapE/view?usp=sharing';
     
     const link = document.createElement('a');
     link.href = resumeUrl;
